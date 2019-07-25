e24570003db400e3612c7b70bdf3c7aeb85ac6aa
Philo <Philo621@gmail.com>
2019-07-25 15:55:21 +0200

    test calculette

---
+++ calc.js
@@ -0,0 +1,9 @@
+function texte() {
+    console.log("test");
+}
+let button = document.getElementById("button");
+for (let i = 0; i < button.length; i++); {
+
+    button[i].addEventListener("click", texte)
+
+}
